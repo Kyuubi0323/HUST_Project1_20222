650b6f38ff9e8f5ee73769be09877bcfece46077
Kyuubi <khoi.nv202647@sis.hust.edu.vn>
2023-05-13 16:50:15 +0700

    update lib

--- MPU6050.h
+++ MPU6050.h
@@ -114,29 +114,67 @@ extern "C" {
 #define MPU6050_CLOCK_PLL_EXT32K        0x04
 #define MPU6050_CLOCK_PLL_EXT19M        0x05
 #define MPU6050_CLOCK_KEEP_RESET        0x07
+//cfg accel
+#define MPU6050_ACCEL_FS_2              0x00
+#define MPU6050_ACCEL_FS_4              0x01
+#define MPU6050_ACCEL_FS_8              0x02
+#define MPU6050_ACCEL_FS_16             0x03
+//cfg gyro
+#define MPU6050_GYRO_FS_250             0x00
+#define MPU6050_GYRO_FS_500             0x01
+#define MPU6050_GYRO_FS_1000            0x02
+#define MPU6050_GYRO_FS_2000            0x03
 
-#define MPU6050_ACCEL_FS_2          0x00
-#define MPU6050_ACCEL_FS_4          0x01
-#define MPU6050_ACCEL_FS_8          0x02
-#define MPU6050_ACCEL_FS_16         0x03
+#define PWR1_DEVICE_RESET               7
+#define PWR1_SLEEP                      6
+#define PWR1_CYCLE                      5
+#define PWR1_TEMP_DIS                   3
+#define PWR1_CLKSEL                     2
+#define PWR1_CLKSEL_LENGTH              3
 
-#define MPU6050_GYRO_FS_250         0x00
-#define MPU6050_GYRO_FS_500         0x01
-#define MPU6050_GYRO_FS_1000        0x02
-#define MPU6050_GYRO_FS_2000        0x03
+#define PWR2_LP_WAKE_CTRL_BIT           7
+#define PWR2_LP_WAKE_CTRL_LENGTH        2
+#define PWR2_STBY_XA                    5
+#define PWR2_STBY_YA                    4
+#define PWR2_STBY_ZA                    3
+#define PWR2_STBY_XG                    2
+#define PWR2_STBY_YG                    1
+#define PWR2_STBY_ZG                    0
 
-#define PWR2_LP_WAKE_CTRL_BIT       7
-#define PWR2_LP_WAKE_CTRL_LENGTH    2
-#define PWR2_STBY_XA                5
-#define PWR2_STBY_YA                4
-#define PWR2_STBY_ZA                3
-#define PWR2_STBY_XG                2
-#define PWR2_STBY_YG                1
-#define PWR2_STBY_ZG                0
+#define MPU6050_TC_PWR_MODE_BIT         7
+#define MPU6050_TC_OFFSET_BIT           6
+#define MPU6050_TC_OFFSET_LENGTH        6
+#define MPU6050_TC_OTP_BNK_VLD_BIT      0
 
+#define MPU6050_VDDIO_LEVEL_VLOGIC      0
+#define MPU6050_VDDIO_LEVEL_VDD         1
 
+#define MPU6050_CFG_EXT_SYNC_SET_BIT    5
+#define MPU6050_CFG_EXT_SYNC_SET_LENGTH 3
+#define MPU6050_CFG_DLPF_CFG_BIT        2
+#define MPU6050_CFG_DLPF_CFG_LENGTH     3
 
+#define MPU6050_EXT_SYNC_DISABLED       0x0
+#define MPU6050_EXT_SYNC_TEMP_OUT_L     0x1
+#define MPU6050_EXT_SYNC_GYRO_XOUT_L    0x2
+#define MPU6050_EXT_SYNC_GYRO_YOUT_L    0x3
+#define MPU6050_EXT_SYNC_GYRO_ZOUT_L    0x4
+#define MPU6050_EXT_SYNC_ACCEL_XOUT_L   0x5
+#define MPU6050_EXT_SYNC_ACCEL_YOUT_L   0x6
+#define MPU6050_EXT_SYNC_ACCEL_ZOUT_L   0x7
 
+#define MPU6050_DLPF_BW_256             0x00
+#define MPU6050_DLPF_BW_188             0x01
+#define MPU6050_DLPF_BW_98              0x02
+#define MPU6050_DLPF_BW_42              0x03
+#define MPU6050_DLPF_BW_20              0x04
+#define MPU6050_DLPF_BW_10              0x05
+#define MPU6050_DLPF_BW_5               0x06
+
+#define MPU6050_GCONFIG_FS_SEL_BIT      4
+#define MPU6050_GCONFIG_FS_SEL_LENGTH   2
+
+#define 
 typedef struct {
 
     int16_t Accel_X_RAW;
@@ -172,6 +210,14 @@ typedef struct {
 void MPU6050_Init(I2C_HandleTypeDef *hi2c);
 bool MPU6050__TestConnection(void);
 
+void MPU6050_Read_Accel(I2C_HandleTypeDef *hi2c, MPU6050_t *DataStruct);
+void MPU6050_Read_Gyro(I2C_HandleTypeDef *hi2c, MPU6050_t *DataStruct);
+void MPU6050_Read_Temp(I2C_HandleTypeDef *hi2c, MPU6050_t *DataStruct);
+void MPU6050_Read_All(I2C_HandleTypeDef *hi2c, MPU6050_t *DataStruct);
+
+double Kalman_getAngle(Kalman_t *Kalman, double newAngle, double newRate, double dt);
+
+/*
 uint8_t MPU6050_GetFullScaleGyroRange();
 void MPU6050_SetFullScaleGyroRange(uint8_t range);
 
@@ -195,11 +241,11 @@ void MPU6050_ReadBit(uint8_t slaveAddr, uint8_t regAddr, uint8_t bitNum, uint8_t
 
 void MPU6050_I2C_ByteWrite(uint8_t slaveAddr, uint8_t *pBuffer, uint8_t writeAddr);
 void MPU6050_I2C_BufferRead(uint8_t slaveAddr, uint8_t *pBuffer, uint8_t readAddr, uint16_t NumBytetoRead);
-
-
+*/
 
 #ifdef __cplusplus
 }
 #endif
+
 
 #endif /* __MPU6050_H */
